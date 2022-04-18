120fa40355e37b72756e8f7509a77875cc8768b2
LimJongOh <kipm@naver.com>
2022-04-18 11:21:13 +0900

    Apply View

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,9 @@ class App extends Component {
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 100);
-    
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err));
   }
 
   callApi = async () => {
